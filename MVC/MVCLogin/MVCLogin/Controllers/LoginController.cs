8aaa7f6995411cb5fcf1ae4134004619b0b58824
ribubrar <ribubrar1@gmail.com>
2019-04-20 22:41:51 +1200

    MVC web app

---
+++ MVC/MVCLogin/MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLogin.Models;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(MVCLogin.Models.User userModel)
+        {
+            using (LoginDatabaseEntities db = new LoginDatabaseEntities())
+            {
+                var userDetails = db.Users.Where(x => x.Username == userModel.Username && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Invalid username or password";
+                    return View("Index",userModel);
+                }
+                else
+                {
+                    Session["userId"] = userDetails.UserId;
+                    Session["username"] = userDetails.Username;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+           
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ MVC/MvcCrud/MvcCrud/Controllers/CustomerController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCrud.Models;
+using System.Data.Entity;
+
+namespace MvcCrud.Controllers
+{
+    public class CustomerController : Controller
+    {
+        DbModels db = new DbModels();
+        // GET: Customer
+        public ActionResult Index()
+        {            
+            return View(db.Customers.ToList());
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(db.Customers.Where(x => x.Id == id).FirstOrDefault());
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(db.Customers.Where(x => x.Id == id).FirstOrDefault());
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            
+            return View(db.Customers.Where(x=> x.Id==id).FirstOrDefault());
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection formCollection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Customer customer = db.Customers.Where(x => x.Id == id).FirstOrDefault();
+                db.Customers.Remove(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
